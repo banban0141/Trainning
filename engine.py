7c18773ba9dbf1ebe5ac2fde78074e39cd6167f9
banban0141 <139753099+banban0141@users.noreply.github.com>
2023-09-28 22:22:14 +0700

    engine.py

--- engine.py
+++ engine.py
@@ -66,59 +66,59 @@ def train_step(model: torch.nn.Module,
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
-def test_step(model: torch.nn.Module, 
-              dataloader: torch.utils.data.DataLoader, 
-              loss_fn: torch.nn.Module,
-              device: torch.device) -> Tuple[float, float]:
-    """Tests a PyTorch model for a single epoch.
+# def test_step(model: torch.nn.Module, 
+#               dataloader: torch.utils.data.DataLoader, 
+#               loss_fn: torch.nn.Module,
+#               device: torch.device) -> Tuple[float, float]:
+#     """Tests a PyTorch model for a single epoch.
 
-    Turns a target PyTorch model to "eval" mode and then performs
-    a forward pass on a testing dataset.
+#     Turns a target PyTorch model to "eval" mode and then performs
+#     a forward pass on a testing dataset.
 
-    Args:
-    model: A PyTorch model to be tested.
-    dataloader: A DataLoader instance for the model to be tested on.
-    loss_fn: A PyTorch loss function to calculate loss on the test data.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+#     Args:
+#     model: A PyTorch model to be tested.
+#     dataloader: A DataLoader instance for the model to be tested on.
+#     loss_fn: A PyTorch loss function to calculate loss on the test data.
+#     device: A target device to compute on (e.g. "cuda" or "cpu").
 
-    Returns:
-    A tuple of testing loss and testing accuracy metrics.
-    In the form (test_loss, test_accuracy). For example:
+#     Returns:
+#     A tuple of testing loss and testing accuracy metrics.
+#     In the form (test_loss, test_accuracy). For example:
 
-    (0.0223, 0.8985)
-    """
-    # Put model in eval mode
-    model.eval() 
+#     (0.0223, 0.8985)
+#     """
+#     # Put model in eval mode
+#     model.eval() 
 
-    # Setup test loss and test accuracy values
-    test_loss, test_acc = 0, 0
+#     # Setup test loss and test accuracy values
+#     test_loss, test_acc = 0, 0
 
-    # Turn on inference context manager
-    with torch.inference_mode():
-        # Loop through DataLoader batches
-        for batch, (X, y) in enumerate(dataloader):
-            # Send data to target device
-            X, y = X.to(device), y.to(device)
+#     # Turn on inference context manager
+#     with torch.inference_mode():
+#         # Loop through DataLoader batches
+#         for batch, (X, y) in enumerate(dataloader):
+#             # Send data to target device
+#             X, y = X.to(device), y.to(device)
 
-            # 1. Forward pass
-            test_pred_logits = model(X)
+#             # 1. Forward pass
+#             test_pred_logits = model(X)
 
-            # 2. Calculate and accumulate loss
-            loss = loss_fn(test_pred_logits, y)
-            test_loss += loss.item()
+#             # 2. Calculate and accumulate loss
+#             loss = loss_fn(test_pred_logits, y)
+#             test_loss += loss.item()
 
-            # Calculate and accumulate accuracy
-            test_pred_labels = test_pred_logits.argmax(dim=1)
-            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+#             # Calculate and accumulate accuracy
+#             test_pred_labels = test_pred_logits.argmax(dim=1)
+#             test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
 
-    # Adjust metrics to get average loss and accuracy per batch 
-    test_loss = test_loss / len(dataloader)
-    test_acc = test_acc / len(dataloader)
-    return test_loss, test_acc
+#     # Adjust metrics to get average loss and accuracy per batch 
+#     test_loss = test_loss / len(dataloader)
+#     test_acc = test_acc / len(dataloader)
+#     return test_loss, test_acc
 
 def train(model: torch.nn.Module, 
           train_dataloader: torch.utils.data.DataLoader, 
-          test_dataloader: torch.utils.data.DataLoader, 
+          # test_dataloader: torch.utils.data.DataLoader, 
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
@@ -157,8 +157,8 @@ def train(model: torch.nn.Module,
     # Create empty results dictionary
     results = {"train_loss": [],
                "train_acc": [],
-               "test_loss": [],
-               "test_acc": []
+               # "test_loss": [],
+               # "test_acc": []
     }
     
     # Make sure model on target device
@@ -171,24 +171,24 @@ def train(model: torch.nn.Module,
                                           loss_fn=loss_fn,
                                           optimizer=optimizer,
                                           device=device)
-        test_loss, test_acc = test_step(model=model,
-          dataloader=test_dataloader,
-          loss_fn=loss_fn,
-          device=device)
+        # test_loss, test_acc = test_step(model=model,
+        #   dataloader=test_dataloader,
+        #   loss_fn=loss_fn,
+        #   device=device)
 
         # Print out what's happening
         print(
           f"Epoch: {epoch+1} | "
-          f"train_loss: {train_loss:.4f} | "
+          # f"train_loss: {train_loss:.4f} | "
           f"train_acc: {train_acc:.4f} | "
-          f"test_loss: {test_loss:.4f} | "
+          # f"test_loss: {test_loss:.4f} | "
           f"test_acc: {test_acc:.4f}"
         )
 
         # Update results dictionary
-        results["train_loss"].append(train_loss)
+        # results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
-        results["test_loss"].append(test_loss)
+        # results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
 
     # Return the filled results at the end of the epochs
